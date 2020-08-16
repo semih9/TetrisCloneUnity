83c85edb44d862eb574226faeca7bf560f875852
semih9 <61362582+semih9@users.noreply.github.com>
2020-08-16 21:54:49 +0300

    First Step
    All progress so far

---
+++ Tetris 2/Tetris II/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameMenu : MonoBehaviour
+{
+    public Text levelText;
+    public Text highScoreText;
+    public Text highScoreText2;
+    public Text highScoreText3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelText.text = "0";
+        highScoreText.text = PlayerPrefs.GetInt("highscore").ToString();
+        highScoreText2.text = PlayerPrefs.GetInt("highscore2").ToString();
+        highScoreText3.text = PlayerPrefs.GetInt("highscore3").ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        if (Game.startingLevel == 0)
+            Game.startingAtLevelZero = true;
+        else
+            Game.startingAtLevelZero = false;
+
+        SceneManager.LoadScene("Level");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void ChangedValue(float value)
+    {
+        Game.startingLevel = (int)value;
+        levelText.text = value.ToString();
+    }
+
+    public void LunchGameMenu()
+    {
+        SceneManager.LoadScene("GameMenu");
+    }
+}
---
+++ Tetris 2/Tetris II/Assets/Scripts/Tetromino.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour
+{
+    float fall = 0;
+    private float fallSpeed;
+    public bool allowRotation = true;
+    public bool limitRotation = false;
+
+    public AudioClip moveSound;
+    public AudioClip rotateSound;
+    public AudioClip landSound;
+
+    private float continuousVerticalSpeed = 0.05f;
+    private float continuousHorizontalSpeed = 0.1f;
+    private float buttonDownWaitMax = 0.2f;
+
+    private float verticalTimer = 0;
+    private float horizontalTimer = 0;
+    private float buttonDownWaitTimerHorizontal = 0;
+    private float buttonDownWaitTimerVertical = 0;
+
+    private bool moveImmediateHorizontal = false;
+    private bool moveImmediateVertical = false;
+
+    public int individualScore = 100;
+
+    private AudioSource audioSource;
+
+    private float individualScoreTime;
+
+    //Variables for touch movements
+    private int touchSensitivityHorizontal = 8;
+    private int touchSensitivityVertical = 4;
+
+    Vector2 previousUnitPosition = Vector2.zero;
+    Vector2 direction = Vector2.zero;
+
+    bool moved = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Game.isPaused)
+        {
+            CheckUserInput();
+
+            UpdateIndividualScore();
+
+            UpdateFallSpeed();
+        }
+
+    }
+
+    void UpdateFallSpeed()
+    {
+        fallSpeed = Game.fallSpeed;
+    }
+
+    void UpdateIndividualScore()
+    {
+        if (individualScoreTime < 1)
+        {
+            individualScoreTime += Time.deltaTime;
+        }
+        else
+        {
+            individualScoreTime = 0;
+
+            individualScore = Mathf.Max(individualScore - 10, 0);
+        }
+    }
+
+    void CheckUserInput(){
+
+        #if UNITY_IOS
+        if(Input.touchCount > 0)
+        {
+            Touch t = Input.GetTouch(0);
+
+            if(t.phase == TouchPhase.Began)
+            {
+                previousUnitPosition = new Vector2(t.position.x, t.position.y);
+            }
+            else if(t.phase == TouchPhase.Moved)
+            {
+                Vector2 touchDeltaPosition = t.deltaPosition;
+                direction = touchDeltaPosition.normalized;
+
+                if(Mathf.Abs(t.position.x - previousUnitPosition.x) >= touchSensitivityHorizontal && direction.x < 0 && t.deltaPosition.y > -10 && t.deltaPosition.y < 10)
+                {
+                    // Move Left
+                    MoveLeft();
+                    previousUnitPosition = t.position;
+                    moved = true;
+                }
+                else if(Mathf.Abs(t.position.x - previousUnitPosition.x) >= touchSensitivityHorizontal && direction.x > 0 && t.deltaPosition.y > -10 && t.deltaPosition.y < 10)
+                {
+                    // Move Right
+                    MoveRight();
+                    previousUnitPosition = t.position;
+                    moved = true;
+                }
+                else if(Mathf.Abs(t.position.y - previousUnitPosition.y) >= touchSensitivityVertical && direction.y < 0 && t.deltaPosition.x >-10 && t.deltaPosition.x < 10)
+                {
+                    // Moving Down
+                    MoveDown();
+                    previousUnitPosition = t.position;
+                    moved = true;
+                }
+            }
+            else if(t.phase == TouchPhase.Ended)
+            {
+                if(!moved && t.position.x > Screen.width)
+                {
+                    Rotate();
+                }
+
+                moved = false;
+            }
+        }
+        #else
+        if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow) )
+        {
+            moveImmediateHorizontal = false;
+            horizontalTimer = 0;
+            buttonDownWaitTimerHorizontal = 0;
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            moveImmediateVertical = false;
+            verticalTimer = 0;
+            buttonDownWaitTimerVertical = 0;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Rotate();
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
+        {
+            MoveDown();
+        }
+
+        if(Time.time - fall >= fallSpeed)
+        {
+            MoveDown();
+        }
+
+        #endif
+
+    }
+
+    void MoveRight()
+    {
+        if (moveImmediateHorizontal)
+        {
+            if (buttonDownWaitTimerHorizontal < buttonDownWaitMax)
+            {
+                buttonDownWaitTimerHorizontal += Time.deltaTime;
+                return;
+            }
+
+            if (horizontalTimer < continuousHorizontalSpeed)
+            {
+                horizontalTimer += Time.deltaTime;
+                return;
+            }
+        }
+
+        if (!moveImmediateHorizontal)
+            moveImmediateHorizontal = true;
+
+        horizontalTimer = 0;
+
+        transform.position += new Vector3(1, 0, 0);
+
+        if (CheckIsValidPosition())
+        {
+            FindObjectOfType<Game>().GridUpdate(this);
+            PlayMoveAudio();
+        }
+        else
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+    }
+
+    void MoveLeft()
+    {
+        if (moveImmediateHorizontal)
+        {
+            if (buttonDownWaitTimerHorizontal < buttonDownWaitMax)
+            {
+                buttonDownWaitTimerHorizontal += Time.deltaTime;
+                return;
+            }
+
+            if (horizontalTimer < continuousHorizontalSpeed)
+            {
+                horizontalTimer += Time.deltaTime;
+                return;
+            }
+        }
+
+        if (!moveImmediateHorizontal)
+            moveImmediateHorizontal = true;
+
+        horizontalTimer = 0;
+
+        transform.position += new Vector3(-1, 0, 0);
+
+        if (CheckIsValidPosition())
+        {
+            FindObjectOfType<Game>().GridUpdate(this);
+            PlayMoveAudio();
+        }
+        else
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+    }
+
+    void MoveDown()
+    {
+        if (moveImmediateVertical)
+        {
+            if (buttonDownWaitTimerVertical < buttonDownWaitMax)
+            {
+                buttonDownWaitTimerVertical += Time.deltaTime;
+                return;
+            }
+
+            if (verticalTimer < continuousVerticalSpeed)
+            {
+                verticalTimer += Time.deltaTime;
+                return;
+            }
+        }
+
+        if (!moveImmediateVertical)
+            moveImmediateVertical = true;
+
+        verticalTimer = 0;
+
+        transform.position += new Vector3(0, -1, 0);
+
+        if (CheckIsValidPosition())
+        {
+            FindObjectOfType<Game>().GridUpdate(this);
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                PlayMoveAudio();
+            }
+        }
+        else
+        {
+            transform.position += new Vector3(0, 1, 0);
+
+            FindObjectOfType<Game>().DeleteRow();
+
+            if (FindObjectOfType<Game>().CheckIsAboveGrid(this))
+            {
+                FindObjectOfType<Game>().GameOver();
+            }
+            PlayLandAudio();
+            FindObjectOfType<Game>().SpawnNextTetromino();
+
+            Game.currentScore += individualScore;
+
+            enabled = false;
+        }
+
+        fall = Time.time;
+    }
+
+    void Rotate()
+    {
+        if (allowRotation)
+        {
+            if (limitRotation)
+            {
+                if (transform.rotation.eulerAngles.z >= 90)
+                {
+                    transform.Rotate(0, 0, -90);
+                }
+                else
+                {
+                    transform.Rotate(0, 0, 90);
+                }
+            }
+            else
+            {
+                transform.Rotate(0, 0, 90);
+            }
+            if (CheckIsValidPosition())
+            {
+                FindObjectOfType<Game>().GridUpdate(this);
+
+                PlayRotateAudio();
+            }
+            else
+            {
+                if (limitRotation)
+                {
+                    if (transform.rotation.eulerAngles.z >= 90)
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, 90);
+                    }
+                }
+                else
+                {
+                    transform.Rotate(0, 0, -90);
+                }
+
+            }
+
+        }
+    }
+
+    void PlayMoveAudio()
+    {
+        audioSource.PlayOneShot(moveSound);
+    }
+
+    void PlayRotateAudio()
+    {
+        audioSource.PlayOneShot(rotateSound);
+    }
+
+    void PlayLandAudio()
+    {
+        audioSource.PlayOneShot(landSound);
+    }
+
+    bool CheckIsValidPosition()
+    {
+        foreach(Transform mino in transform)
+        {
+            Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
+            if (FindObjectOfType<Game>().CheckInsideGrid(pos) == false)
+            {
+                return false;
+            }
+
+            if(FindObjectOfType<Game>().GetTransformAtGridPosition(pos) != null && FindObjectOfType<Game>().GetTransformAtGridPosition(pos).parent != transform)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
